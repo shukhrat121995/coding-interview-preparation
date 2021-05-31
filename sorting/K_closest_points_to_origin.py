092b3015e39d17bab9e9e8166964c49e4d655be6
unknown <shux121995@gmail.com>
2021-05-31 09:39:12 +0500

    K closest points to origin

---
+++ sorting/K_closest_points_to_origin.py
@@ -0,0 +1,14 @@
+"""
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k,
+
+return the k closest points to the origin (0, 0).
+
+The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+"""
+
+
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        return sorted(points, key=lambda x : x[0]**2 + x[1]**2)[:k]
