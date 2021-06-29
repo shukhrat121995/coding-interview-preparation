6f0b4920582ce5a35ce6981f81b8a3545b088be1
unknown <shux121995@gmail.com>
2021-06-29 10:18:55 +0500

    binary tree right side view definition added

--- tree/binary_tree_right_side_view.py
+++ tree/binary_tree_right_side_view.py
@@ -1,3 +1,20 @@
+"""
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you
+can see ordered from top to bottom.
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Input: root = [1,null,3]
+Output: [1,3]
+
+Input: root = []
+Output: []
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+"""
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
